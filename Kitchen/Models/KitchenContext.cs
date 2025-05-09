207cf63bda4ff4f8f2b53bbad3946b857b2f6208
Hassan Swelam <hasanswelam@outlook.com>
2025-05-09 14:41:03 +0300

    f

--- Kitchen/Models/KitchenContext.cs
+++ Kitchen/Models/KitchenContext.cs
@@ -9,6 +9,8 @@ namespace Kitchen.Models
         public DbSet<Account> Accounts { get; set; }
         public DbSet<AccountRole> AccountRoles { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public KitchenContext()
         {
             
@@ -17,10 +19,6 @@ namespace Kitchen.Models
         {
             
         }
-        public DbSet<Dish> Dishes { get; set; }
-        public DbSet<Order> Orders { get; set; }
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-KRVT3AP;Initial Catalog=Kitchen;Integrated Security=True;Encrypt=False");
