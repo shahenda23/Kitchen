2809ebdeab5b506e5f66a37c50a70033bb33cd39
k.m <k.m@DESKTOP-KRVT3AP>
2025-05-09 14:06:50 +0300

    add IRep

---
+++ Kitchen/Controllers/DishController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kitchen.Controllers
+{
+    public class DishController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
