18e45b7572498187faa654ac5414e22fa6621af7
Shahenda <shahendakhaled267@gmail.com>
2025-05-09 14:04:32 +0300

    .

---
+++ Kitchen/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3.Repository
+{
+    public interface IRepository<T>
+    {
+        List <T> GetAll(string includes="");
+        T GetById(int id, string includes="" );
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(int id);
+        void Save();
+
+    }
+}
