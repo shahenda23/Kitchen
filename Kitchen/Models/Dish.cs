bfdcda7cbc1e08ea56b24ff0747bff1312a55eb3
k.m <k.m@DESKTOP-KRVT3AP>
2025-05-09 12:35:06 +0300

    add two models

---
+++ Kitchen/Models/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kitchen.Models
+{
+    public class Dish
+    {
+        public int Dish_ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        [ForeignKey("Chef")]
+        public int Chef_ID { get; set; }
+        public List<Feedback> Feedbacks { get; set; }
+        public Chef Chef { get; set; }
+
+
+    }
+}
