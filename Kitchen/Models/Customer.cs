559fe1ecec61ac126420606480a92eb8d7ec5770
Shahenda <shahendakhaled267@gmail.com>
2025-05-09 12:47:57 +0300

    ..

--- Kitchen/Models/Customer.cs
+++ Kitchen/Models/Customer.cs
@@ -3,9 +3,9 @@
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string? CustomerName { get; set; }
-        public string? CustomerAddress { get; set; }
-        public string? CustomerPhone { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
         public string? CustomerEmail { get; set; }
 
     }
