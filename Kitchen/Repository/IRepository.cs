443af1f9e7694b7364844fed6b582f44fceb2d41
Hassan Swelam <hasanswelam@outlook.com>
2025-05-09 14:31:03 +0300

    g

--- Kitchen/Repository/IRepository.cs
+++ Kitchen/Repository/IRepository.cs
@@ -2,5 +2,11 @@
 {
     public interface IRepository<T>
     {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T obj);
+        void Edit(T obj);
+        void Delete(int id);
+        void Save();
     }
 }
